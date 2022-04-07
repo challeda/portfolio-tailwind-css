afeee68dfcf666c73dff5dcd419be87cee94b776
challeda <mistech68@gmail.com>
2022-04-07 22:11:39 +0800

    contoh portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320",
+      },
+    },
+  },
+  plugins: [],
+};
